8b351522650e8e820f077033e5721b415c7e0612
Akash Gupta <agupta18@wayfair.com>
2024-04-30 01:19:24 +0530

    commit

---
+++ src/main/java/com/interview/preparation/low_level_design/blocking_queues/Consumer.java
@@ -0,0 +1,23 @@
+package com.interview.preparation.low_level_design.blocking_queues;
+
+public class Consumer implements Runnable{
+  private final CustomBlockingQueue<Integer> customBlockingQueue;
+  private final int poisonPill;
+  public Consumer(CustomBlockingQueue<Integer> customBlockingQueue, int poisonPill) {
+    this.customBlockingQueue = customBlockingQueue;
+    this.poisonPill = poisonPill;
+  }
+  @Override
+  public void run() {
+    while (true) {
+      try {
+        Integer number = customBlockingQueue.deQueue();
+        if (number.equals(poisonPill)) {
+          return;
+        }
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+}
