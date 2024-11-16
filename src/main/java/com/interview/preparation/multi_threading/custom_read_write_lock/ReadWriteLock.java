9f417000c0b460966d07e10480adc21126aaea63
ak9578ash <akashgupta9578@gmail.com>
2024-11-16 16:27:57 +0530

    update

---
+++ src/main/java/com/interview/preparation/multi_threading/custom_read_write_lock/ReadWriteLock.java
@@ -0,0 +1,52 @@
+package com.interview.preparation.multi_threading.custom_read_write_lock;
+
+/**
+ * Conditions for given read or write access
+ * Read Access    If no threads are writing, and no threads have requested write access.
+ * Write Access   If no threads are reading or writing.
+ */
+public class ReadWriteLock {
+  private int readers;
+  private int writers;
+  private int writeRequests;
+
+  public ReadWriteLock() {
+    this.writers = 0;
+    this.readers = 0;
+    this.writeRequests = 0;
+  }
+
+  public synchronized void readLock() {
+    while (writers > 0 || writeRequests > 0) {
+      try {
+        this.wait();
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
+    readers++;
+  }
+
+  public synchronized void readUnlock() {
+    readers--;
+    notifyAll();
+  }
+
+  public synchronized void writeLock() {
+    writeRequests++;
+    while (readers > 0 || writers > 0) {
+      try {
+        this.wait();
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
+    writeRequests--;
+    writers++;
+  }
+
+  public synchronized void writeUnlock() {
+    writers--;
+    notifyAll();
+  }
+}
