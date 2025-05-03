e561a7a9dada79152ce5ad93f876f985ceadbcbc
ak9578ash <akashgupta9578@gmail.com>
2025-05-04 00:13:38 +0530

    commit

--- src/main/java/com/interview/preparation/low_level_design/cab_booking/service/CabAvailabilityService.java
+++ src/main/java/com/interview/preparation/low_level_design/cab_booking/service/CabAvailabilityService.java
@@ -26,7 +26,10 @@ public class CabAvailabilityService {
         unavailableCabs.addAll(lockedCabs);
         return unavailableCabs;
     }
-
+    /*
+      1. There could be a use case where we have to return available cars based on some criteria so for that we can
+         add search strategy and use that to filter the cabs.
+    */
     public List<Cab> getAvailableCabs() {
         List<Cab> allCabs = cabService.getAllCabs();
         List<Cab> unavailableCabs = getUnavailableCabs();
