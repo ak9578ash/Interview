a57a5b88dfbae2df35b3ff8d73abb9a3a7a17ed1
ak9578ash <akashgupta9578@gmail.com>
2025-06-26 15:40:37 +0530

    commit

--- src/main/java/com/interview/preparation/low_level_design/testing/services/UrlContentDeDuplicator.java
+++ src/main/java/com/interview/preparation/low_level_design/testing/services/UrlContentDeDuplicator.java
@@ -1,25 +1,25 @@
-package com.interview.preparation.low_level_design.testing.services;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class UrlContentDeDuplicator {
-  private final Set<String> visitedUrls;
-  private final Set<Integer> contentHashes;
-
-  public UrlContentDeDuplicator() {
-    this.visitedUrls = new HashSet<>();
-    this.contentHashes = new HashSet<>();
-  }
-
-  //TODO:could add the strategy pattern to allow different de-duplication strategies rather then using the default hashCode
-  public boolean isDuplicate(String url, String content) {
-    int hash = content.hashCode();
-    if (visitedUrls.contains(url) || contentHashes.contains(hash)) {
-      return true;
-    }
-    visitedUrls.add(url);
-    contentHashes.add(hash);
-    return false;
-  }
-}
+//package com.interview.preparation.low_level_design.testing.services;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//public class UrlContentDeDuplicator {
+//  private final Set<String> visitedUrls;
+//  private final Set<Integer> contentHashes;
+//
+//  public UrlContentDeDuplicator() {
+//    this.visitedUrls = new HashSet<>();
+//    this.contentHashes = new HashSet<>();
+//  }
+//
+//  //TODO:could add the strategy pattern to allow different de-duplication strategies rather then using the default hashCode
+//  public boolean isDuplicate(String url, String content) {
+//    int hash = content.hashCode();
+//    if (visitedUrls.contains(url) || contentHashes.contains(hash)) {
+//      return true;
+//    }
+//    visitedUrls.add(url);
+//    contentHashes.add(hash);
+//    return false;
+//  }
+//}
